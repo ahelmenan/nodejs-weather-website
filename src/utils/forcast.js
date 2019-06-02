4dfdef0fa07d73a9d82a55979b37ff3a3810a2e5
ahmed elmenan <ah.elmenan@gmail.com>
2019-06-02 13:42:58 +0000

    adding day's summary and timezone

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -3,8 +3,8 @@ const request = require('request');
 const forcast = (lat, long, callback) => {
     const url = 'https://api.darksky.net/forecast/6423b2093712ff3250a7473c482779dc/'+encodeURIComponent(lat)+','+encodeURIComponent(long)+'?units=si';
     request({ url: url, json: true}, (error, {body} = {}) => {
-        const {currently} = body;
-        const {temperature, precipProbability} = currently;
+        const {currently, timezone} = body;
+        const {summary, temperature, precipProbability} = currently;
         if (error)
                 callback('unable to access the Weather API', undefined);
         else if (body.error)
@@ -12,7 +12,7 @@ const forcast = (lat, long, callback) => {
         else
             
                 callback(undefined, 
-                 { sum :`it's  ${temperature}° it's ${precipProbability}% raining`});
+                 { sum :`${summary} it's currently  ${temperature}° it's ${precipProbability}% raining , timezone : ${timezone}`});
             })
 };
 
